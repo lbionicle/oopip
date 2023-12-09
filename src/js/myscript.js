9da6a8fefcbd4247e19a2deec6924a6f78458e5d
Nikolay <example@gmail.com>
2023-12-10 01:55:11 +0300

    2-sec

--- src/js/myscript.js
+++ src/js/myscript.js
@@ -1,4 +1,117 @@
-//====================================================================================================
+//CARD_GOODS====================================================================================================
+
+class GoodsCard {
+    constructor(src, title, price, parentSelector, attr, ...classes) {
+        this.src = src;
+        this.title = title;
+        this.price = price;
+        this.classes = classes;
+        this.attr = attr;
+        this.parent = document.querySelector(parentSelector);
+    }
+
+
+    render() {
+        const element = document.createElement('button');
+
+        if (this.classes.length === 0) {
+            this.classes = "goods-element";
+            element.classList.add(this.classes);
+            element.setAttribute(this.attr, "")
+        } else {
+            this.classes.forEach(className => {
+                element.classList.add(className)
+                element.setAttribute(this.attr, "")
+            });
+        }
+
+        element.innerHTML = `
+        <div class="element-img">
+            <img src="${this.src}" alt="">
+        </div>
+        <div class="element-info">
+            <div class="element-info-name">${this.title}</div>
+            <div class="element-info-price">$${this.price}</div>
+        </div>
+        `;
+        this.parent.append(element);
+    }
+}
+
+new GoodsCard("../img/goods1.png", "Stacking pyramid", "10.00", ".goods-elements" , "data-goods", "goods-element").render();
+new GoodsCard("../img/goods3.png", "Mini basketball", "9.00", ".goods-elements" , "data-goods", "goods-element").render();
+new GoodsCard("../img/goods4.png", "Robot Transformer", "29.00", ".goods-elements" , "data-goods", "goods-element").render();
+
+//POPUP_GOODS====================================================================================================
+
+// class BigGoodsCard {
+//     constructor(src, title, price, article, stock, descr, parentSelector, ...classes) {
+//         this.src = src;
+//         this.title = title;
+//         this.article = article;
+//         this.stock = stock;
+//         this.descr = descr;
+//         this.price = price;
+//         this.classes = classes;
+//         this.attr = attr;
+//         this.parent = document.querySelector(parentSelector);
+//     }
+
+
+//     render() {
+//         const element = document.createElement('div');
+
+//         if (this.classes.length === 0) {
+//             this.classes = "popup__content";
+//             element.classList.add(this.classes);
+//             element.setAttribute(this.attr, "")
+//         } else {
+//             this.classes.forEach(className => {
+//                 element.classList.add(className)
+//                 element.setAttribute(this.attr, "")
+//             });
+//         }
+//         element.innerHTML = `
+//         <button data-close type="button" class="popup__close"><img src="../img/close.svg" alt=""></button>
+//         <div class="popup-content-img">
+//             <img class="popup-content-img" src="${this.src}" alt="">
+//         </div>
+//         <div class="popup-content-info">
+//             <div class="popup-content-title">${this.title}</div>
+//             <div class="popup-content-article">Article: <span>${this.article}</span></div>
+//             <div class="popup-content-stock">Stock: <span>${this.stock}</span></div>
+//             <div class="popup-content-price">$${this.price}</div>
+//             <div class="popup-divider"></div>
+//             <div class="popup-content-titledescr">Short Description:</div>
+//             <div class="popup-content-subdescrp">${this.descr}</div>
+//             <div class="popup-content-orderpanel">
+//                 <div class="orderpanel-count">
+//                     <button class="orderpanel-removeGoods"><img src="../img/minus.svg" alt=""></button>
+//                     <div class="orderpanel-countGoods">1</div>
+//                     <button class="orderpanel-addGoods"><img src="../img/plus.svg" alt=""></button>
+//                 </div>
+//                 <button class="orderpanel-goOrder">Buy now</button>
+//                 <button class="orderpanel-cart">Add to cart</button>
+//             </div>
+//         </div>
+//         </div>
+//         `;
+//         this.parent.innerHTML = ``;
+//         this.parent.append(element);
+//     }
+// }
+
+function renderPopup(src, title, price, article, stock, descr) {
+    document.querySelector("img.popup-content-img").src = src;
+    document.querySelector(".popup-content-title").textContent = title;
+    document.querySelector(".popup-content-price").textContent = "$" + price;
+    document.querySelector(".popup-content-article > span").textContent = article;
+    document.querySelector(".popup-content-stock > span").textContent = stock;
+    document.querySelector(".popup-content-subdescrp").textContent = descr;
+    document.querySelector(".orderpanel-countGoods").textContent = 1;
+}
+//GOODS====================================================================================================
+
 const btnFilterAge = document.querySelectorAll(".filter-item");
 const btnCategories = document.querySelectorAll(".goods-category-item");
 const pagination = document.querySelectorAll(".row-pagination-elements > *");
@@ -66,13 +179,28 @@ pagination.forEach((btn) => {
 
 show(btnFilterAge, "active");
 show(btnCategories, "category-active");
-//====================================================================================================
+
+//POPUP====================================================================================================
+
 
 const popup = document.querySelectorAll('[data-goods]');
 const addGoods = document.querySelector(".orderpanel-addGoods");
 const removeGoods = document.querySelector(".orderpanel-removeGoods");
 const countGoods = document.querySelector(".orderpanel-countGoods");
 const stockGoods = document.querySelector(".popup-content-stock");
+const cartGood = document.querySelectorAll(".cart-header")
+
+const userAuth = document.querySelectorAll(".user-header");
+
+function addPopup(elemPop, data, id) {
+    elemPop.forEach((elem) => {
+        elem.setAttribute(data, id)
+        elem.classList.add("link")
+        elem.addEventListener("click", () => {
+            renderPopup("../img/biggoods1.png", "LEGO bricks set", "20.00", "1995751877966", "13", "This 234-piece set includes bright and colorful LEGO-style bricks in a variety of shapes and sizes. Build anything from simple structures to elaborate townscapes to boost creativity. These chunky plastic bricks snap satisfyingly together and pull easily apart for frustration-free construction play. Big enough for little hands but still compatible with major brands, this budget-friendly builder set makes the perfect STEM learning toy.");
+        })
+    })
+}
 
 addGoods.addEventListener("click", () => {
     if (parseInt(countGoods.textContent.match(/\d+/)) != parseInt(stockGoods.textContent.match(/\d+/))) {
@@ -86,8 +214,51 @@ removeGoods.addEventListener("click", () => {
     }
 })
 
-popup.forEach((elem) => {
-    elem.setAttribute("data-popup", "#popup")
-    elem.classList.add("link")
+addPopup(cartGood, "data-popup", "#popup-cart")
+addPopup(popup, "data-popup", "#popup-goods")
+addPopup(userAuth, "data-popup", "#popup-auth")
+console.log(userAuth)
+//REGISTER====================================================================================================
+
+const enter_password = document.querySelectorAll(".auth-enter-password");
+const login_popup = document.getElementById("login-auth-user");
+const reg_popup = document.getElementById("reg-auth-user");
+const auth_login_btns = document.querySelectorAll(".login-auth")  
+const auth_reg_btns = document.querySelectorAll(".register-auth")  
+
+const btn_log = document.querySelector("#btn-hide-log");
+const btn_reg = document.querySelector("#btn-hide-reg");
+
+
+auth_login_btns.forEach((elem) => {
+    elem.addEventListener("click", () => {
+        login_popup.style.display = "flex";
+        reg_popup.style.display = "none";
+    })
 })
-console.log(addGoods)
+
+auth_reg_btns.forEach((elem) => {
+    elem.addEventListener("click", () => {
+        login_popup.style.display = "none";
+        reg_popup.style.display = "flex";
+    })
+})
+
+enter_password.forEach((elem) => {
+    elem.addEventListener("click", (e) => {
+        if (e.target.id == "btn-hide-log") {
+            e.preventDefault();
+            const log_1 = document.querySelector("#log-1");
+
+            log_1.type == "password" ? log_1.type = "text" : log_1.type = "password";
+            log_1.type == "password" ? btn_log.classList.remove("show-auth") : btn_log.classList.add("show-auth");
+        } else if (e.target.id == "btn-hide-reg") {
+            e.preventDefault();
+            const reg_1 = document.querySelector("#reg-1")
+
+            reg_1.type == "password" ? reg_1.type = "text" : reg_1.type = "password";        
+            reg_1.type == "password" ? btn_reg.classList.remove("show-auth") : btn_reg.classList.add("show-auth");
+        }
+    }) 
+})
+//====================================================================================================
